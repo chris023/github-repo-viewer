d81029379297f893a6d53a393bfe6e9527923283
Chris Meyer <chris.meyer023@gmail.com>
2020-11-04 15:21:59 -0700

    Template routing

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import { Switch, Redirect, Route } from 'react-router-dom'
+
+import { RepoDetails } from './RepoDetails'
+import { Search } from './Search'
+
 const App = () => {
-  return <div />
+  return (
+    <Switch>
+      <Route exact path="/search" component={Search} />
+      <Route exact path="/repository" component={RepoDetails} />
+      <Redirect to="/search" />
+    </Switch>
+  )
 }
 
 export default App
