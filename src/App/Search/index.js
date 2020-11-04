d70e84ee59ba675a41fdb51f8010cc57c7f2b1f2
Chris Meyer <chris.meyer023@gmail.com>
2020-11-04 16:09:24 -0700

    Implement searching for responses

--- src/App/Search/index.js
+++ src/App/Search/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { SearchInput } from '../../Components'
 import { useStyles } from './style'
@@ -6,9 +6,16 @@ import { useStyles } from './style'
 const Search = () => {
   const classes = useStyles()
 
+  // eslint-disable-next-line no-unused-vars
+  const [results, setResults] = useState({
+    loading: false,
+    error: false,
+    data: undefined,
+  })
+
   return (
     <div className={classes.root}>
-      <SearchInput />
+      <SearchInput setResults={setResults} />
     </div>
   )
 }
--- src/Components/SearchInput/index.js
+++ src/Components/SearchInput/index.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { InputBase, IconButton, Paper } from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search'
 
+import { searchGithubRepos } from '../../utils'
 import { useStyles } from './style'
 
-const SearchInput = () => {
+const SearchInput = ({ setResults }) => {
   const classes = useStyles()
+  const [query, setQuery] = useState('')
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    setResults((prev) => ({ ...prev, loading: true }))
+
+    searchGithubRepos(query)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => setResults((prev) => ({ ...prev, error })))
+      .finally(() => setResults((prev) => ({ ...prev, loading: false })))
+  }
 
   return (
-    <Paper component="form" className={classes.root}>
+    <Paper component="form" className={classes.root} onSubmit={onSubmit}>
       <InputBase
         className={classes.input}
         placeholder="Search Github Repositories"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
       <IconButton type="submit" className={classes.iconButton}>
         <SearchIcon />
