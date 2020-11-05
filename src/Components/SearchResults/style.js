d1c863c90041ebd12902707d549c04322b227ed8
Chris Meyer <chris.meyer023@gmail.com>
2020-11-04 17:28:22 -0700

    Add title

--- src/Components/SearchResults/style.js
+++ src/Components/SearchResults/style.js
@@ -2,16 +2,15 @@ import { makeStyles } from '@material-ui/core'
 
 export const useStyles = makeStyles((theme) => ({
   root: {
-    alignItems: 'center',
     boxSizing: 'border-box',
-    display: 'flex',
-    flexDirection: 'column',
     marginTop: theme.spacing(2),
-    minHeight: 76,
     maxWidth: 600,
     width: '100%',
   },
   list: {
     width: '100%',
+  },
+  title: {
+    marginBottom: theme.spacing(2),
   },
 }))
