ed727147c6aa3f8635ff87ded246e2cfd081f6b9
chris023 <chris.meyer023@gmail.com>
2020-11-08 21:02:33 -0700

    Template a few tests

--- cypress/integration/Default/index.js
+++ cypress/integration/Default/index.js
@@ -1,6 +1,69 @@
-describe('Server Status', () => {
-  it('Should load search page', () => {
-    cy.visit('/')
-    cy.findByText(/Useless But Sick Repo Searcher/i).should('be.visible')
+const sizes = ['iphone-6', 'ipad-2', 'macbook-13', 'macbook-16']
+
+sizes.forEach((size) => {
+  describe(size, () => {
+    beforeEach(() => {
+      cy.viewport(size)
+    })
+
+    describe('Server Status', () => {
+      it(`Should load search page [${size}]`, () => {
+        cy.viewport(size)
+        cy.visit('/')
+        cy.findByText(/Useless But Sick Repo Searcher/i).should('be.visible')
+        cy.findByTestId('cy-search-input').should('be.visible')
+      })
+    })
+
+    describe('Search', () => {
+      beforeEach(() => {
+        cy.viewport(size)
+        cy.visit('/search')
+      })
+
+      it('Should render search bar ', () => {})
+
+      it('Should be able to get search results with default filters', () => {})
+
+      it('Should be able to select language', () => {})
+    })
+
+    describe('Sorting, Filtering & Paging', () => {
+      beforeEach(() => {
+        cy.viewport(size)
+        cy.visit('/search')
+      })
+
+      it('Should be able change result sorting', () => {})
+
+      it('Should be able to change page size', () => {})
+
+      it('Should be able to go to next page', () => {})
+
+      it('Should be able to go back to previous page', () => {})
+    })
+
+    describe('Routing', () => {
+      beforeEach(() => {
+        cy.viewport(size)
+        cy.visit('/search')
+      })
+
+      it('Should default path to /search', () => {})
+
+      it('Should load repository detailed page on search result click', () => {})
+    })
+
+    describe('Repository Details', () => {
+      beforeEach(() => {
+        cy.viewport(size)
+      })
+
+      it(`Should render repository details`, () => {})
+
+      it(`Should render repository or owner's image`, () => {})
+
+      it('Should be able to redirect to repository page on GitHub', () => {})
+    })
   })
 })
